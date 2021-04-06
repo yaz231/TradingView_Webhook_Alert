62bf99a777ec5c1c5ebf2014ac9cdf57e5366edd
yaz231 <yazan_alatrach@hotmail.com>
2021-04-06 01:50:20 -0600

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-pro_coinbase_API_passphrase = 'fstucpg6z9u'
-b64secret = 'xErcUFTbZZTenafnRuWyZPB5cAMvOni5NTvz/YO3/zcnI56p66WEhuejtonNt/Y/24T8wRPtINRLvrnopfweug=='
-pro_coinbase_key = 'ec115e18af7633d64b086a2988226d3e'
-sandbox_secret = '5b9nw2236xg'
-sandbox_key = '01d8957c6b2243359e0fb6e31d18a935'
-sandbox_b64secret = 'mPaHSH7xZevvzu2KMEpDoYJGn3Yle/RFJSeodbh0adg0nlOVFhEjRHNH1o1xJZ1aw7yblbZwvL9/8Njb4VziYA=='
-ALPACA_API_KEY = 'PKITSAMNHYI1TP7EVP27'
-ALPACA_SECRET_KEY = 'LQPzzu8Qc48eHh1EC75xO9WFDzmJgNQOYO6BtWoL'
-
